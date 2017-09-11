74138054ecba703a61518fe13ab55535f128b859
Samuel Gruetter <samuel.gruetter@epfl.ch>
2017-09-11 19:38:45 -0400

    add nat lemmas from FSCQ's Word.v to NatLib.v

--- NatLib.v
+++ NatLib.v
@@ -119,3 +119,176 @@ Theorem div2_bound : forall k n,
   omega.
 Qed.
 
+Lemma pow2_add_mul: forall a b,
+  pow2 (a + b) = (pow2 a) * (pow2 b).
+Proof.
+  induction a; destruct b; firstorder; simpl.
+  repeat rewrite Nat.add_0_r.
+  rewrite Nat.mul_1_r; auto.
+  repeat rewrite Nat.add_0_r.
+  rewrite IHa.
+  simpl.
+  repeat rewrite Nat.add_0_r.
+  rewrite Nat.mul_add_distr_r; auto.
+Qed.
+
+Lemma mult_pow2_bound: forall a b x y,
+  x < pow2 a -> y < pow2 b -> x * y < pow2 (a + b).
+Proof.
+  intros.
+  rewrite pow2_add_mul.
+  apply Nat.mul_lt_mono_nonneg; omega.
+Qed.
+
+Lemma mult_pow2_bound_ex: forall a c x y,
+  x < pow2 a -> y < pow2 (c - a) -> c >= a -> x * y < pow2 c.
+Proof.
+  intros.
+  replace c with (a + (c - a)) by omega.
+  apply mult_pow2_bound; auto.
+Qed.
+
+Lemma lt_mul_mono' : forall c a b,
+  a < b -> a < b * (S c).
+Proof.
+  induction c; intros.
+  rewrite Nat.mul_1_r; auto.
+  rewrite Nat.mul_succ_r.
+  apply lt_plus_trans.
+  apply IHc; auto.
+Qed.
+
+Lemma lt_mul_mono : forall a b c,
+  c <> 0 -> a < b -> a < b * c.
+Proof.
+  intros.
+  replace c with (S (c - 1)) by omega.
+  apply lt_mul_mono'; auto.
+Qed.
+
+Lemma zero_lt_pow2 : forall sz, 0 < pow2 sz.
+Proof.
+  induction sz; simpl; omega.
+Qed.
+
+Lemma one_lt_pow2:
+  forall n,
+    1 < pow2 (S n).
+Proof.
+  intros.
+  induction n.
+  simpl; omega.
+  remember (S n); simpl.
+  omega.
+Qed.
+
+Lemma mul2_add : forall n, n * 2 = n + n.
+Proof.
+  induction n; firstorder.
+Qed.
+
+Lemma pow2_le_S : forall sz, (pow2 sz) + 1 <= pow2 (sz + 1).
+Proof.
+  induction sz; simpl; auto.
+  repeat rewrite Nat.add_0_r.
+  rewrite pow2_add_mul.
+  repeat rewrite mul2_add.
+  pose proof (zero_lt_pow2 sz).
+  omega.
+Qed.
+
+Lemma pow2_bound_mono: forall a b x,
+  x < pow2 a -> a <= b -> x < pow2 b.
+Proof.
+  intros.
+  replace b with (a + (b - a)) by omega.
+  rewrite pow2_add_mul.
+  apply lt_mul_mono; auto.
+  pose proof (zero_lt_pow2 (b - a)).
+  omega.
+Qed.
+
+
+Lemma pow2_inc : forall n m,
+  0 < n -> n < m ->
+    pow2 n < pow2 m.
+Proof.
+  intros.
+  generalize dependent n; intros.
+  induction m; simpl.
+  intros. inversion H0.
+  unfold lt in H0.
+  rewrite Nat.add_0_r.
+  inversion H0.
+  apply Nat.lt_add_pos_r.
+  apply zero_lt_pow2.
+  apply Nat.lt_trans with (pow2 m).
+  apply IHm.
+  exact H2.
+  apply Nat.lt_add_pos_r.
+  apply zero_lt_pow2.
+Qed.
+
+Lemma mod2_S_S : forall n,
+  mod2 (S (S n)) = mod2 n.
+Proof.
+  intros.
+  destruct n; auto; destruct n; auto.
+Qed.
+
+Lemma mod2_S_not : forall n,
+  mod2 (S n) = if (mod2 n) then false else true.
+Proof.
+  intros.
+  induction n; auto.
+  rewrite mod2_S_S.
+  destruct (mod2 n); replace (mod2 (S n)); auto.
+Qed.
+
+Lemma mod2_S_eq : forall n k,
+  mod2 n = mod2 k ->
+  mod2 (S n) = mod2 (S k).
+Proof.
+  intros.
+  do 2 rewrite mod2_S_not.
+  rewrite H.
+  auto.
+Qed.
+
+Theorem drop_mod2_add : forall n k,
+  mod2 (n + 2 * k) = mod2 n.
+Proof.
+  intros.
+  induction n.
+  simpl.
+  rewrite Nat.add_0_r.
+  replace (k + k) with (2 * k) by omega.
+  apply mod2_double.
+  replace (S n + 2 * k) with (S (n + 2 * k)) by omega.
+  apply mod2_S_eq; auto.
+Qed.
+
+Theorem div2_plus_2 : forall n k,
+  div2 (n + 2 * k) = div2 n + k.
+Proof.
+  induction n; intros.
+  simpl.
+  rewrite Nat.add_0_r.
+  replace (k + k) with (2 * k) by omega.
+  apply div2_double.
+  replace (S n + 2 * k) with (S (n + 2 * k)) by omega.
+  destruct (Even.even_or_odd n).
+  - rewrite <- even_div2.
+    rewrite <- even_div2 by auto.
+    apply IHn.
+    apply Even.even_even_plus; auto.
+    apply Even.even_mult_l; repeat constructor.
+
+  - rewrite <- odd_div2.
+    rewrite <- odd_div2 by auto.
+    rewrite IHn.
+    omega.
+    apply Even.odd_plus_l; auto.
+    apply Even.even_mult_l; repeat constructor.
+Qed.
+
