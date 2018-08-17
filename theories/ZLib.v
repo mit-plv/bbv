02ecdc26c4633c0b3a8b0140e103b710e194efdc
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-08-16 20:21:42 -0400

    add Z lemmas and hints

--- theories/ZLib.v
+++ theories/ZLib.v
@@ -207,3 +207,31 @@ Proof.
   rewrite Z.pow_neg_r in H by assumption.
   omega.
 Qed.
+
+Lemma two_digits_encoding_inj_lo: forall base a b c d: Z,
+  0 <= b < base ->
+  0 <= d < base ->
+  base * a + b = base * c + d ->
+  b = d.
+Proof.
+  intros.
+  pose proof Z.mod_unique as P.
+  specialize P with (b := base) (q := c) (r := d).
+  specialize P with (2 := H1).
+  rewrite P by omega.
+  rewrite <- Z.add_mod_idemp_l by omega.
+  rewrite Z.mul_comm.
+  rewrite Z.mod_mul by omega.
+  rewrite Z.add_0_l.
+  rewrite Z.mod_small by omega.
+  reflexivity.
+Qed.
+
+Lemma two_digits_encoding_inj_hi: forall base a b c d: Z,
+  0 <= b < base ->
+  0 <= d < base ->
+  base * a + b = base * c + d ->
+  a = c.
+Proof.
+  intros. nia.
+Qed.
